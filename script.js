311b22688b2cfeeb4b7406201b5ef2c9e249cdb5
Ridwan Dahir <ridwandahir@Ridwans-MacBook-Air-2.local>
2023-10-15 05:56:40 -0500

    solution to interactive resume

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    function hide_element(element, speed){
+        $(element).hide(speed);
+    }
+    function toggle_element(element, speed){
+        $(element).toggle(speed);
+    }
+
+})
